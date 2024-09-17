a4432986ed152156488b7b0bb2f562967ada2d12
Alex <126792810+AxxlLN@users.noreply.github.com>
2024-09-17 22:34:33 +0300

    First commit

---
+++ src/main/java/com/example/parsingBot/ParsingBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.parsingBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsingBotApplication.class, args);
+	}
+
+}
